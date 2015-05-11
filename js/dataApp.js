d4360619dd9a6006aa849c6cc505a8bd7de40509
Thomas Delaney <thomasdelaney@webinnovate.ie>
2015-05-11 11:24:15 +0100

    Update
    update

--- js/dataApp.js
+++ js/dataApp.js
@@ -16,7 +16,7 @@ Code was reused from the MDW labs. Factories and http for access to JSON files o
 
 */
 
-var dataApp = angular.module('dataApp', []) // sets up the module
+var dataApp = angular.module('dataApp', ['ngRoute']) // sets up the module
 	
 	
 	dataApp.factory('dataFactory', function($http){ // chains the factory to the app
@@ -25,7 +25,7 @@ var dataApp = angular.module('dataApp', []) // sets up the module
 			
 			getDataAsync: function(callback){
 				$http.get('data.json').success(callback); // provides access to the JSON file 
-				
+				console.log($scope.data);
 				}
 			};
 		
@@ -40,55 +40,7 @@ var dataApp = angular.module('dataApp', []) // sets up the module
 		}); 
 	
 	
-	$scope.addItem = function(itemText){ // Add to $scope.todo (assuming it's an array of objects)
-        
-        $scope.todo.push({"title": itemText, "done": false, "priority": $scope.priority, "date" : $scope.enteredDate });
-		console.log($scope.todo);
-		$scope.newItem = ""; // clears the input box
-		$scope.priority = "";
-		
-		};// end of $scope.clearCompleted function
-		
 	
-	$scope.clearCompleted = function(){// code reuse from https://www.youtube.com/watch?v=FSOiVprDdj0
-		
-		$scope.todo = $scope.todo.filter(function(item){
-			
-			return !item.done // returns only the active items to the list
-			
-			})
-			
-			}; // end of $scope.addItem function
-			
-			
-			
-	$scope.remaining = function() { // Code reused from http://codepen.io/pibby/pen/DLtaK/
-			
-				var count = 0;
-				angular.forEach($scope.todo, function(todo){
-					count+= todo.done ? 0 : 1;
-				});
-				
-				return count;
-			}; // end of $scope.remaining function
-			
-	
-	$scope.archive = function() { // saves completed item to a todoDone object
-		
-            var oldTodo = $scope.todo;
-            
-            angular.forEach(oldTodo, function(todo) {
-              if (todo.done) $scope.todoDone.push(todo);
-			  
-            });
-			$scope.clearCompleted(); // call function to clear completed items
-			//console.log( $scope.todoDone);
-          };			
-
-	$scope.predicate = 'date';	// sets sort by date on load or refresh
-	$scope.todoDone = []; // used to archive done items
-	$scope.options = ["High","Medium","Low","Someday","None"]; // Priority options from the $scope
-	$scope.selectedOption = $scope.options;
 	
 	
 	});
